74bc8c7bb84691dd5a3df458182a5e64fc1ad6f0
itonedeveloper <itonedeveloper@gmail.com>
2020-02-05 10:30:22 -0500

    [dir][file]{page}{index.js}=> create index.js for first page  ; use for React-route-dom !!!!

---
+++ src/components/Page/index/Index.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+export default class Index extends React.Component {
+    constructor() {
+        super();
+    }
+
+    render() {
+        let element =
+            (
+                <React.Fragment>
+                    Index Page
+                </React.Fragment>
+            );
+        return element
+    }
+}
