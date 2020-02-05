9bf50b170b881aecccf0b188ef138bf43df4747c
itonedeveloper <itonedeveloper@gmail.com>
2020-02-05 09:48:38 -0500

    [file]{App.js}=>write class for root component

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export default class App extends React.Component {
+    constructor() {
+        super();
+    }
+    render() {
+        let element =
+            (
+                <React.Fragment>
+
+                </React.Fragment>
+            );
+        return element
+    }
+}
