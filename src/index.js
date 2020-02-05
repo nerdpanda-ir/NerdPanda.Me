8e227d2bd7cf4e2f1a1725ad7638edbef3a18dcb
itonedeveloper <itonedeveloper@gmail.com>
2020-02-05 10:19:37 -0500

    [file]{index.js}=> update App component Address in {index.js }

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@
 
     import React from 'react';
     import ReactDom from 'react-dom';
-    import App from './App';
+    import App from './components/App/App';
 
 /* ===     IMPORT SCRIPTS     ===*/
 
