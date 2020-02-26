86a14ff7188f5ea884596acc80caca67711b67c0
itonedeveloper <itonedeveloper@gmail.com>
2020-02-26 09:20:49 -0500

    [FILE]{Header.js}-> create header component

---
+++ src/components/Page/index/Header/Header.js
@@ -0,0 +1,18 @@
+/*====   import Sctipt    ====*/
+    import React from 'react';
+/*====   import Sctipt    ====*/
+export default class Header extends React.Component
+{
+    render()
+    {
+        let result = 
+        (
+            <React.Fragment>
+                <header id='pageHeader'> 
+                    page header 
+                </header>
+            </React.Fragment>
+        );
+        return result; 
+    }
+}
