5d35e8f6d06f49d8af502b46f96ed1b22027db2f
itonedeveloper <itonedeveloper@gmail.com>
2020-02-26 08:15:14 -0500

    [file]{NotFound}-> create Not Found Page

---
+++ src/components/Page/NotFound/NotFound.js
@@ -0,0 +1,16 @@
+/*====   import Sctipt    ====*/
+import React from 'react';
+/*====   import Sctipt    ====*/
+export default class NotFound extends React.Component
+{
+    render()
+    {
+        let result = 
+        (
+            <React.Fragment>
+                <h1>  404 Not Found </h1>
+            </React.Fragment>
+        );
+        return result; 
+    }
+}
