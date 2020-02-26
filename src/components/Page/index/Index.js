b8d30ae913dc2165fd82dc0b59feeeef5dce619c
itonedeveloper <itonedeveloper@gmail.com>
2020-02-26 08:21:42 -0500

    [file]{Index.js}-> sort codes

--- src/components/Page/index/Index.js
+++ src/components/Page/index/Index.js
@@ -1,9 +1,12 @@
-import React from "react";
-import '../../../css/Page/index/index.css'
+/*====   import Sctipt    ====*/
+    import React from "react";
+/*====   import Sctipt    ====*/
+
+/*====   import Styles    ====*/
+    import '../../../css/Page/index/index.css'
+/*====   import Styles    ====*/
+
 export default class Index extends React.Component {
-    constructor() {
-        super();
-    }
     render() {
         let element =
             (
