25c2e944787a78f980b2a690f1685fa7cc0d8d05
itonedeveloper <itonedeveloper@gmail.com>
2020-02-27 09:14:47 -0500

    [file]{Nav.js}-> import nav css and import Logo Component

--- src/components/Page/index/Header/Nav/Nav.js
+++ src/components/Page/index/Header/Nav/Nav.js
@@ -1,7 +1,12 @@
 /*====   import Sctipt    ====*/
     import React from "react";  
     import Container from '../../../../Container/Container'
+    import Logo from '../../../../Logo/Logo'
 /*====   import Sctipt    ====*/
+
+/*====   import Styles  ====*/
+    import '../../../../../css/Page/index/Header/Nav/Nav.css'
+/*====   import Styles  ====*/
 export default class Nav extends React.Component
 {
     render()
@@ -9,9 +14,9 @@ export default class Nav extends React.Component
         let result = 
         (
             <React.Fragment>
-                <nav id='topNav'>
+                <nav id='topNav' className='sh1'>
                     <Container className='h100'>
-                        Container
+                        <Logo id='NavLogo' className='h100 disIB col-1' imageSrc='image' alt='NerdPandaLogo'/>
                     </Container>
                 </nav>
             </React.Fragment>
