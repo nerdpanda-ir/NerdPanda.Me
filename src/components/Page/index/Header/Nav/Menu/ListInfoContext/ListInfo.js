91e9246fbdf51f715425b0835a60329ba5944b66
itonedeveloper <itonedeveloper@gmail.com>
2020-02-29 03:37:44 -0500

    [File][ListInfoContext.JS]-> create menu  info context

---
+++ src/components/Page/index/Header/Nav/Menu/ListInfoContext/ListInfo.js
@@ -0,0 +1,6 @@
+/*====   import Sctipt    ====*/
+    import React from 'react';
+/*====   import Sctipt    ====*/
+let listInfo = React.createContext(['منو','fab fa-react']);
+export let ListInfoProvider = listInfo.Provider;
+export let ListInfoConsumer = listInfo.Consumer;
