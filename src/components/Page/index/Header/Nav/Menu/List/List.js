5f016d7762c49ac9c8075fa53d0229e07d047d7f
itonedeveloper <itonedeveloper@gmail.com>
2020-02-29 01:47:22 -0500

    [file]{List.js}-> create menu list component

---
+++ src/components/Page/index/Header/Nav/Menu/List/List.js
@@ -0,0 +1,16 @@
+/*====   import Sctipt    ====*/
+import React from 'react';
+/*====   import Sctipt    ====*/
+export default class List extends React.Component
+{
+    render()
+    {
+        let result = 
+        (
+            <React.Fragment>
+                <li>list </li>
+            </React.Fragment>
+        );
+        return result ;
+    }
+}
