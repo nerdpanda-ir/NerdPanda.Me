f6c432f20ddab590c945bbbfdd6dadf9fd1f6bc0
itonedeveloper <itonedeveloper@gmail.com>
2020-02-29 10:54:43 -0500

    [file]{LangIcon.js}-> create langicon component

---
+++ src/components/Page/index/Header/Nav/Options/Language/LangIcon/LangIcon.js
@@ -0,0 +1,17 @@
+/*====   import Sctipt    ====*/
+    import React from "react";
+/*====   import Sctipt    ====*/
+
+export default class LangIcon extends React.Component
+{
+    render()
+    {
+        let result =
+            (
+                <React.Fragment>
+                    icon
+                </React.Fragment>
+            );
+        return result;
+    }
+}
