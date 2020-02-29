c8a3b610ab92ab6e552da83a391046cfc638aedd
itonedeveloper <itonedeveloper@gmail.com>
2020-02-29 10:55:43 -0500

    [file]{Arrow}-> create arrow component

---
+++ src/components/Page/index/Header/Nav/Options/Language/Arrow/Arrow.js
@@ -0,0 +1,15 @@
+/*====   import Sctipt    ====*/
+    import React from "react";
+/*====   import Sctipt    ====*/
+export default class Arrow extends React.Component
+{
+    render() {
+        let result =
+            (
+                <React.Fragment>
+                    <i className="fal fa-sort-down" id='lagnArrow'></i>
+                </React.Fragment>
+            );
+        return result;
+    }
+}
