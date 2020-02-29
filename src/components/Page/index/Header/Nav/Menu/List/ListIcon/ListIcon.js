291e9e91f4baba3164b66d045b45f0886694fdd6
itonedeveloper <itonedeveloper@gmail.com>
2020-02-29 02:02:54 -0500

    [File]{ListIcon.js}-> create list icon component

---
+++ src/components/Page/index/Header/Nav/Menu/List/ListIcon/ListIcon.js
@@ -0,0 +1,16 @@
+/*====   import Sctipt    ====*/
+import React from 'react';
+/*====   import Sctipt    ====*/
+export default class ListIcon extends React.Component
+{
+    render()
+    {
+        let result = 
+        (
+            <React.Fragment>
+                <i class="fab fa-react"></i>
+            </React.Fragment>
+        )
+        return result;
+    }
+}
