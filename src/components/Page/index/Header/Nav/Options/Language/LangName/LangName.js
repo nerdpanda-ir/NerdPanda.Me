7c2ece40be3fecf4288f34a45fce238b92ba09c2
itonedeveloper <itonedeveloper@gmail.com>
2020-02-29 10:53:49 -0500

    [file]{LangName}-> create LangName component

---
+++ src/components/Page/index/Header/Nav/Options/Language/LangName/LangName.js
@@ -0,0 +1,15 @@
+/*====   import Sctipt    ====*/
+    import React from "react";
+/*====   import Sctipt    ====*/
+
+export default class LangName extends React.Component{
+    render() {
+        let result =
+            (
+                <React.Fragment>
+                        name
+                </React.Fragment>
+            );
+            return result;
+    }
+}
