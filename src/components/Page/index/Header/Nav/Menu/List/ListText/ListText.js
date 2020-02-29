b1ae96b8d4c05cf84018fa727ea17fa068be0f8f
itonedeveloper <itonedeveloper@gmail.com>
2020-02-29 02:08:58 -0500

    [file]{ListText.js}-> create list text component

---
+++ src/components/Page/index/Header/Nav/Menu/List/ListText/ListText.js
@@ -0,0 +1,19 @@
+/*====   import Sctipt    ====*/
+import React from 'react';
+import ListIcon from './ListIcon/ListIcon';
+/*====   import Sctipt    ====*/
+export default class ListText extends React.Component
+{
+    render()
+    {
+        let result = 
+        (
+            <React.Fragment>
+                <span className='listText'>
+                    منوی اول 
+                </span>
+            </React.Fragment>
+        );
+        return result;
+    }
+}
