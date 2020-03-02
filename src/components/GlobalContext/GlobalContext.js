1bb9f8263ae35cff09a83766e211f18b13f87edd
itonedeveloper <itonedeveloper@gmail.com>
2020-03-02 07:24:38 -0500

    [file]{GlobalContext.js}-> create globalContext

---
+++ src/components/GlobalContext/GlobalContext.js
@@ -0,0 +1,6 @@
+/*====   import Sctipt    ====*/
+    import React from 'react';
+/*====   import Sctipt    ====*/
+export let GlobalContext = React.createContext();
+export let GlobalContextProvider = GlobalContext.Provider;
+export let GlobalContextConsumer = GlobalContext.Consumer;
