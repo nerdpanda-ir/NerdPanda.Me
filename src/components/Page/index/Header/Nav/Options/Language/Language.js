bce173b08d220834270b3e6475956c76387d2c7d
itonedeveloper <itonedeveloper@gmail.com>
2020-03-02 04:22:53 -0500

    [file]{Language.js}-> import Language icons and import Language Selected COmponent and use width Lang info context

--- src/components/Page/index/Header/Nav/Options/Language/Language.js
+++ src/components/Page/index/Header/Nav/Options/Language/Language.js
@@ -1,10 +1,18 @@
 /*====   import Sctipt    ====*/
     import React from "react";
+    import {LangInfoProvider} from "../LangInfoContext/LangInfo";
+    import LangSelected from "../LangSelected/LangSelected";
 /*====   import Sctipt    ====*/
 
 /*====   import Style    ====*/
     import '../../../../../../../css/Page/index/Header/Nav/Options/Language/Language.css';
 /*====   import Style    ====*/
+
+/*====   import Files    ====*/
+    import persian from '../../../../../../../img/global/Lang/iran.svg';
+    import english from '../../../../../../../img/global/Lang/uk.svg';
+    import arab from '../../../../../../../img/global/Lang/ar.svg';
+/*====   import Files    ====*/
 
 export default class Language extends React.Component
 {
@@ -13,7 +21,9 @@ export default class Language extends React.Component
             (
                 <React.Fragment>
                     <div id='language' className='col-11 sh2 br3 boxSBB dirR bg-white-light sh3'>
-                        lang
+                        <LangInfoProvider value={{img : persian , alt : 'persian-lang' , name : 'فارسی'}}>
+                            <LangSelected />
+                        </LangInfoProvider>
                     </div>
                 </React.Fragment>
             );
