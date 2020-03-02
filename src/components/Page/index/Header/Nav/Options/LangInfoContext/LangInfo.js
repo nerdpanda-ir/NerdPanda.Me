38915a68cd84a1ed02cde02ccd697f2ca385c3b2
itonedeveloper <itonedeveloper@gmail.com>
2020-03-02 04:21:53 -0500

    {file}{LangInfo.js}-> create langInfo Context

---
+++ src/components/Page/index/Header/Nav/Options/LangInfoContext/LangInfo.js
@@ -0,0 +1,6 @@
+/*====   import Sctipt    ====*/
+    import React from "react";
+/*====   import Sctipt    ====*/
+let langInfo = React.createContext({img : '' , alt : '' , name : ''});
+export let LangInfoProvider = langInfo.Provider ;
+export let LangInfoConsumer = langInfo.Consumer ;
