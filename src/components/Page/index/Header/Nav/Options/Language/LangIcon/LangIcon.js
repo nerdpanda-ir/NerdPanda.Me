72d895a3416bf4c803accacdb6fa5dc5036620da
itonedeveloper <itonedeveloper@gmail.com>
2020-03-02 04:23:47 -0500

    [file]{LangIcon.js}-> get value of context

--- src/components/Page/index/Header/Nav/Options/Language/LangIcon/LangIcon.js
+++ src/components/Page/index/Header/Nav/Options/Language/LangIcon/LangIcon.js
@@ -1,5 +1,6 @@
 /*====   import Sctipt    ====*/
     import React from "react";
+    import {LangInfoConsumer} from "../../LangInfoContext/LangInfo";
 /*====   import Sctipt    ====*/
 
 export default class LangIcon extends React.Component
@@ -9,7 +10,9 @@ export default class LangIcon extends React.Component
         let result =
             (
                 <React.Fragment>
-                    icon
+                    <LangInfoConsumer>
+                        {(value)=><img src={value.img} alt={value.alt}/>}
+                    </LangInfoConsumer>
                 </React.Fragment>
             );
         return result;
