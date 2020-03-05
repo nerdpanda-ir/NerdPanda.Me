da0b2982e85cdf07676a0ba53f84778456c9cbac
itonedeveloper <itonedeveloper@gmail.com>
2020-03-05 03:06:30 -0500

    [file]{LangList.js}-> create LangList Component

---
+++ src/components/Page/index/Header/Nav/Options/Language/Langlist/LangList.js
@@ -0,0 +1,17 @@
+/*====   import Sctipt    ====*/
+    import React from "react";
+/*====   import Sctipt    ====*/
+export default class LangList extends React.Component
+{
+    render() {
+        let result =
+            (
+                <React.Fragment>
+                    <ul className='posAb col-12' id='LangList'>
+                        <li>Lang</li>
+                    </ul>
+                </React.Fragment>
+            );
+        return result;
+    }
+}
