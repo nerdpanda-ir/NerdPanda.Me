6e031b18f98d7c9f49a14ccce2e0d7822faf2714
itonedeveloper <itonedeveloper@gmail.com>
2020-03-05 04:21:40 -0500

    [file]{LangList.js}-> import List component and use !!! take a value for globoal context

--- src/components/Page/index/Header/Nav/Options/Language/Langlist/LangList.js
+++ src/components/Page/index/Header/Nav/Options/Language/Langlist/LangList.js
@@ -1,14 +1,35 @@
 /*====   import Sctipt    ====*/
     import React from "react";
+    import List from "./List/List";
+    import {GlobalContextProvider} from "../../../../../../../GlobalContext/GlobalContext";
+
 /*====   import Sctipt    ====*/
+
+
+/*====   import Files    ====*/
+    import persian from '../../../../../../../../img/global/Lang/iran.svg';
+    import english from '../../../../../../../../img/global/Lang/uk.svg';
+    import arab from '../../../../../../../../img/global/Lang/ar.svg';
+/*====   import Files    ====*/
+
+
+
 export default class LangList extends React.Component
 {
     render() {
         let result =
             (
                 <React.Fragment>
-                    <ul className='posAb col-12' id='LangList'>
-                        <li>Lang</li>
+                    <ul className='posAb col-12 bg-white sh2' id='LangList'>
+                        <GlobalContextProvider value={{img : persian , alt : 'persian-lang' , name : 'فارسی'}}>
+                            <List />
+                        </GlobalContextProvider>
+                        <GlobalContextProvider value={{img : english , alt : 'English-lang' , name : 'English'}}>
+                            <List />
+                        </GlobalContextProvider>
+                        <GlobalContextProvider value={{img : arab , alt : 'Arab-lang' , name : 'عربی'}}>
+                            <List />
+                        </GlobalContextProvider>
                     </ul>
                 </React.Fragment>
             );
