67380c5b5c18bb797df1ad5d76c368e90073e8ab
itonedeveloper <itonedeveloper@gmail.com>
2020-03-05 05:49:16 -0500

    [file]{List.js}-> import stylesheet file

--- src/components/Page/index/Header/Nav/Options/Language/Langlist/List/List.js
+++ src/components/Page/index/Header/Nav/Options/Language/Langlist/List/List.js
@@ -2,6 +2,10 @@
     import React from "react";
     import LangRow from "../../LangRow/LangRow";
 /*====   import Sctipt    ====*/
+
+/*====   import Style    ====*/
+    import '../../../../../../../../../css/Page/index/Header/Nav/Options/Language/LangList/List/List.css'
+/*====   import Style    ====*/
 export default class List extends React.Component {
     render() {
         let result =
