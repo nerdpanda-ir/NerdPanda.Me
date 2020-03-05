6d9d20115dcf17c7ba6ae859cceb75d6dd763b51
itonedeveloper <itonedeveloper@gmail.com>
2020-03-05 03:03:45 -0500

    [file]{langSelected}=> import Global context

--- src/components/Page/index/Header/Nav/Options/Language/LangSelected/LangSelected.js
+++ src/components/Page/index/Header/Nav/Options/Language/LangSelected/LangSelected.js
@@ -2,6 +2,8 @@
     import React from "react";
     import LangRow from "../LangRow/LangRow";
     import Arrow from "../Arrow/Arrow";
+    import {GlobalContextProvider} from "../../../../../../../GlobalContext/GlobalContext";
+
 /*====   import Sctipt    ====*/
 
 /*====   import Style    ====*/
@@ -9,13 +11,21 @@
 /*====   import Style    ====*/
 
 
+/*====   import Style    ====*/
+    import persian from "../../../../../../../../img/global/Lang/iran.svg";
+/*====   import Style    ====*/
+
+
+
 export default class LangSelected extends React.Component{
     render() {
         let result = (
             <React.Fragment>
                 <section id='LangSelect' className='h100 textCenter boxSBB'>
+                    <GlobalContextProvider value={{img : persian , alt : 'persian-lang' , name : 'فارسی'}}>
                         <LangRow />
-                        <Arrow />
+                    </GlobalContextProvider>
+                    <Arrow />
                 </section>
             </React.Fragment>
         );
