f064bbd7b71e3fc63f7bbdab9fb5919287de8aa2
itonedeveloper <itonedeveloper@gmail.com>
2020-03-12 12:20:41 -0400

    [file]{speciality.js}=> create text paragraph

--- src/components/Page/index/Header/Speciality/Speciality.js
+++ src/components/Page/index/Header/Speciality/Speciality.js
@@ -19,6 +19,9 @@ export default class Speciality extends React.Component{
                                 <Text className='color-secound' tagName='h1'>
                                     فول استک دولوپر
                                 </Text>
+                                <Text className='color-main' tagName='p'>
+                                    لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است. چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است و برای شرایط فعلی تکنولوژی مورد نیاز و کاربردهای متنوع با هدف بهبود ابزارهای کاربردی می باشد. کتابهای زیادی در شصت و سه درصد گذشته
+                                </Text>
                             </Col>
                         </Container>
                     </section>
