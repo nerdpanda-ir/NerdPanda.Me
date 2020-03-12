6df27a3eb1a6a1314e76f07b578b8f0d3de9ea24
itonedeveloper <itonedeveloper@gmail.com>
2020-03-12 12:18:25 -0400

    [file]{speciality.js}=> modify component name and modify Title component path

--- src/components/Page/index/Header/Speciality/Speciality.js
+++ src/components/Page/index/Header/Speciality/Speciality.js
@@ -2,11 +2,11 @@
     import React from 'react';
     import Container from "../../../../Container/Container";
     import Col from "../../../../items/Col/Col";
+    import Text from "../../../../items/Text/Text";
 /*====   import Sctipt    ====*/
 
 /*====   import Styles    ====*/
     import '../../../../../css/Page/index/Header/Speciality/Speciality.css'
-import Title from "../../../../items/Title/Title";
 /*====   import Styles    ====*/
 export default class Speciality extends React.Component{
     render() {
@@ -16,9 +16,9 @@ export default class Speciality extends React.Component{
                     <section id='speciality' className='boxSBB'>
                         <Container>
                             <Col className='col-6'>
-                                <Title className='color-secound' tagName='h1'>
+                                <Text className='color-secound' tagName='h1'>
                                     فول استک دولوپر
-                                </Title>
+                                </Text>
                             </Col>
                         </Container>
                     </section>
