c64dd00268089f651d5334ab6ce9c4b6c773abf0
itonedeveloper <itonedeveloper@gmail.com>
2020-03-12 09:15:23 -0400

    [file]{Speciality.js}-> create speciality component

---
+++ src/components/Page/index/Header/Speciality/Speciality.js
@@ -0,0 +1,14 @@
+/*====   import Sctipt    ====*/
+    import React from 'react';
+/*====   import Sctipt    ====*/
+export default class Speciality extends React.Component{
+    render() {
+        let result =
+            (
+                <React.Fragment>
+                    this is speciality
+                </React.Fragment>
+            );
+        return result;
+    }
+}
