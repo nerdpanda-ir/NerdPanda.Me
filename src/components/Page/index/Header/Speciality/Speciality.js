be720005b554f198d148ad4f322a214c6ead3cae
itonedeveloper <itonedeveloper@gmail.com>
2020-03-12 09:39:13 -0400

    [file]{Speciality.js}-> add Speciality css file !!! and create Speciality wrapper html element

--- src/components/Page/index/Header/Speciality/Speciality.js
+++ src/components/Page/index/Header/Speciality/Speciality.js
@@ -1,12 +1,18 @@
 /*====   import Sctipt    ====*/
     import React from 'react';
 /*====   import Sctipt    ====*/
+
+/*====   import Styles    ====*/
+    import '../../../../../css/Page/index/Header/Speciality/Speciality.css'
+/*====   import Styles    ====*/
 export default class Speciality extends React.Component{
     render() {
         let result =
             (
                 <React.Fragment>
-                    this is speciality
+                    <section id='speciality'>
+
+                    </section>
                 </React.Fragment>
             );
         return result;
