5b05ad34f3b5bc4aeb640cb9b19c42529f84a687
itonedeveloper <itonedeveloper@gmail.com>
2020-03-12 11:25:00 -0400

    [file]{Col.js}-> set props to state in component

--- src/components/items/Col/Col.js
+++ src/components/items/Col/Col.js
@@ -2,12 +2,22 @@
     import React from "react";
 /*====   import Sctipt    ====*/
 export default class Col extends React.Component {
+    constructor() {
+        super();
+        this.state = {body :'' ,classList : ''};
+    }
+    static getDerivedStateFromProps(props)
+    {
+        return {body : props.children , classList:   props.className };
+    }
     render() {
         let result =
             (
                 <React.Fragment>
-                    <section>
-                        hi im col
+                    <section className={this.state.classList}>
+                        {
+                            this.state.body
+                        }
                     </section>
                 </React.Fragment>
             );
