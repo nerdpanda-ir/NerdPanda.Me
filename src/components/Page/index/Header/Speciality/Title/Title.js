2687d1ed89715777999a8d1226f888d0ea5122cd
itonedeveloper <itonedeveloper@gmail.com>
2020-03-12 09:52:01 -0400

    [file]{Title}=> create spiciality title component

---
+++ src/components/Page/index/Header/Speciality/Title/Title.js
@@ -0,0 +1,15 @@
+/*====   import Sctipt    ====*/
+import React from 'react';
+/*====   import Sctipt    ====*/
+
+export default class Title extends React.Component{
+    render() {
+        let result =
+            (
+                <React.Fragment>
+                    <h1 id='specialityTitle'> فول استک دولوپر</h1>
+                </React.Fragment>
+            );
+        return result;
+    }
+}
