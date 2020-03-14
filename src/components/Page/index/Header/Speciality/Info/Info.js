0c7488c0a4faffc640839dc20647a29e7eead9ec
itonedeveloper <itonedeveloper@gmail.com>
2020-03-14 03:46:40 -0400

    [file]{Info.js}-> create speciality info component

---
+++ src/components/Page/index/Header/Speciality/Info/Info.js
@@ -0,0 +1,14 @@
+/*====   import Sctipt    ====*/
+    import React from 'react';
+/*====   import Sctipt    ====*/
+export default class Info extends React.Component{
+    render() {
+        let result =
+            (
+                <React.Fragment>
+                    info
+                </React.Fragment>
+            );
+        return result;
+    }
+}
