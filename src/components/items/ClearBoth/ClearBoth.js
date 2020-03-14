a8888efbae46ba73fc9bdc29ebf9dfe5a8a47847
itonedeveloper <itonedeveloper@gmail.com>
2020-03-14 07:21:58 -0400

    [file]{ClearBoth}= > add clearBoth component

---
+++ src/components/items/ClearBoth/ClearBoth.js
@@ -0,0 +1,14 @@
+/*====   import Sctipt    ====*/
+    import React from "react";
+/*====   import Sctipt    ====*/
+export default class ClearBoth extends React.Component{
+    render() {
+        let result =
+            (
+                <React.Fragment>
+                    <div className='clearBoth'></div>
+                </React.Fragment>
+            );
+        return result;
+    }
+}
