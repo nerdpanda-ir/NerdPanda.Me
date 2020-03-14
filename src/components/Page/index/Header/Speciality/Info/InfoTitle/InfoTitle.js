a9421b3f4509bc80b1cf08cc6cfe712face0b323
itonedeveloper <itonedeveloper@gmail.com>
2020-03-14 04:50:59 -0400

    [file]{InfoTitle.js}-> import Text Component

--- src/components/Page/index/Header/Speciality/Info/InfoTitle/InfoTitle.js
+++ src/components/Page/index/Header/Speciality/Info/InfoTitle/InfoTitle.js
@@ -1,12 +1,13 @@
 /*====   import Sctipt    ====*/
     import React from 'react';
+    import Text from "../../../../../../items/Text/Text";
 /*====   import Sctipt    ====*/
 export default class InfoTitle extends React.Component{
     render() {
         let result =
             (
                 <React.Fragment>
-                    title
+                    <Text tagName='h1' className='color-secound'>فول استک دولوپر</Text>
                 </React.Fragment>
             );
         return result;
