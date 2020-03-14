ba2ec0fdbea58ff538a56fac1d13aa062dc8a311
itonedeveloper <itonedeveloper@gmail.com>
2020-03-14 03:51:46 -0400

    [file]{InfoTitle.js}-> create title component for speciality

---
+++ src/components/Page/index/Header/Speciality/Info/InfoTitle/InfoTitle.js
@@ -0,0 +1,14 @@
+/*====   import Sctipt    ====*/
+    import React from 'react';
+/*====   import Sctipt    ====*/
+export default class InfoTitle extends React.Component{
+    render() {
+        let result =
+            (
+                <React.Fragment>
+                    title
+                </React.Fragment>
+            );
+        return result;
+    }
+}
