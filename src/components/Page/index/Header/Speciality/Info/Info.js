6711125aba15e3ff32ac63cb6b00139720303538
itonedeveloper <itonedeveloper@gmail.com>
2020-03-14 04:35:03 -0400

    [file]{Info.js}-> add component col and infoTitle and InfoDescription and use !!!!

--- src/components/Page/index/Header/Speciality/Info/Info.js
+++ src/components/Page/index/Header/Speciality/Info/Info.js
@@ -1,12 +1,18 @@
 /*====   import Sctipt    ====*/
     import React from 'react';
+    import Col from "../../../../../items/Col/Col";
+    import InfoTitle from "./InfoTitle/InfoTitle";
+    import InfoDescription from "./InfoDescription/InfoDescription";
 /*====   import Sctipt    ====*/
 export default class Info extends React.Component{
     render() {
         let result =
             (
                 <React.Fragment>
-                    info
+                    <Col className='col-6 disIB'>
+                        <InfoTitle />
+                        <InfoDescription />
+                    </Col>
                 </React.Fragment>
             );
         return result;
