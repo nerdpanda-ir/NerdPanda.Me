1b2b0946be395f9244a057aedd42a64303976d53
itonedeveloper <itonedeveloper@gmail.com>
2020-03-14 06:16:23 -0400

    [file]{Col.js}=> set id props to state and use for element

--- src/components/items/Col/Col.js
+++ src/components/items/Col/Col.js
@@ -4,17 +4,17 @@
 export default class Col extends React.Component {
     constructor() {
         super();
-        this.state = {body :'' ,classList : ''};
+        this.state = {body :'' ,classList : '' , id : ''};
     }
     static getDerivedStateFromProps(props)
     {
-        return {body : props.children , classList:   props.className };
+        return {body : props.children , classList:   props.className  , id :props.id};
     }
     render() {
         let result =
             (
                 <React.Fragment>
-                    <section className={this.state.classList}>
+                    <section className={this.state.classList} id={this.state.id}>
                         {
                             this.state.body
                         }
