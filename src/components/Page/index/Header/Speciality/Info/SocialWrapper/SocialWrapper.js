ac281250b68188ad4b044c62afa52c5fca31fcbb
itonedeveloper <itonedeveloper@gmail.com>
2020-03-14 08:27:53 -0400

    [file]{socialWrapper}-> create social wrapepr component

---
+++ src/components/Page/index/Header/Speciality/Info/SocialWrapper/SocialWrapper.js
@@ -0,0 +1,16 @@
+/*====   import Sctipt    ====*/
+    import React from 'react';
+/*====   import Sctipt    ====*/
+export default class SocialWrapper extends React.Component{
+    render() {
+        let result =
+            (
+                <React.Fragment>
+                    <section id='socialRow'>
+                        Social
+                    </section>
+                </React.Fragment>
+            );
+        return result;
+    }
+}
