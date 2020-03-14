c1bdea7b5da67c25b5712630537aa8c5e51283a6
itonedeveloper <itonedeveloper@gmail.com>
2020-03-14 03:55:27 -0400

    [file]{InfoDescription.js}-> create description component for speciality

---
+++ src/components/Page/index/Header/Speciality/Info/InfoDescription/InfoDescription.js
@@ -0,0 +1,14 @@
+/*====   import Sctipt    ====*/
+    import React from 'react';
+/*====   import Sctipt    ====*/
+export default class InfoDescription extends React.Component{
+    render() {
+        let result =
+            (
+                <React.Fragment>
+                    description
+                </React.Fragment>
+            );
+        return result;
+    }
+}
