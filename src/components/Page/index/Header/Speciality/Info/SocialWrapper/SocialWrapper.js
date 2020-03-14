d6e606b740353484ecf6e859e65ce4ea3fa9f840
itonedeveloper <itonedeveloper@gmail.com>
2020-03-14 08:52:57 -0400

    [file]{SocialWrapper}-> create btn and import text Component

--- src/components/Page/index/Header/Speciality/Info/SocialWrapper/SocialWrapper.js
+++ src/components/Page/index/Header/Speciality/Info/SocialWrapper/SocialWrapper.js
@@ -1,5 +1,6 @@
 /*====   import Sctipt    ====*/
     import React from 'react';
+    import Text from "../../../../../../items/Text/Text";
 /*====   import Sctipt    ====*/
 export default class SocialWrapper extends React.Component{
     render() {
@@ -7,7 +8,10 @@ export default class SocialWrapper extends React.Component{
             (
                 <React.Fragment>
                     <section id='socialRow'>
-                        Social
+                        <Text className='bg-dark-perple color-white br3' tagName='span' id='contactMeBtn'>
+                            تماس با من
+                            <i className="fal fa-long-arrow-alt-left"></i>
+                        </Text>
                     </section>
                 </React.Fragment>
             );
