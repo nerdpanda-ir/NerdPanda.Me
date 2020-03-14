e11be8c02ccfdd2bd3d02a03579d33bf907579dd
itonedeveloper <itonedeveloper@gmail.com>
2020-03-14 04:31:05 -0400

    [file]{Speciality.js}-> import Info Component and remove trash code !!!

--- src/components/Page/index/Header/Speciality/Speciality.js
+++ src/components/Page/index/Header/Speciality/Speciality.js
@@ -1,8 +1,7 @@
 /*====   import Sctipt    ====*/
     import React from 'react';
     import Container from "../../../../Container/Container";
-    import Col from "../../../../items/Col/Col";
-    import Text from "../../../../items/Text/Text";
+    import Info from "./Info/Info";
 /*====   import Sctipt    ====*/
 
 /*====   import Styles    ====*/
@@ -15,14 +14,7 @@ export default class Speciality extends React.Component{
                 <React.Fragment>
                     <section id='speciality' className='boxSBB'>
                         <Container>
-                            <Col className='col-6 disIB'>
-                                <Text className='color-secound' tagName='h1'>
-                                    فول استک دولوپر
-                                </Text>
-                                <Text className='color-main' tagName='p'>
-                                    لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است. چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است و برای شرایط فعلی تکنولوژی مورد نیاز و کاربردهای متنوع با هدف بهبود ابزارهای کاربردی می باشد. کتابهای زیادی در شصت و سه درصد گذشته
-                                </Text>
-                            </Col>
+                            <Info />
                         </Container>
                     </section>
                 </React.Fragment>
