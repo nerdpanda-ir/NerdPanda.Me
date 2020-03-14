33d107cce32a453c871f890cdabceb81e7688f9a
itonedeveloper <itonedeveloper@gmail.com>
2020-03-14 04:58:55 -0400

    [file]{InfoDescription}->import Text Component and  create paragraph

--- src/components/Page/index/Header/Speciality/Info/InfoDescription/InfoDescription.js
+++ src/components/Page/index/Header/Speciality/Info/InfoDescription/InfoDescription.js
@@ -1,12 +1,13 @@
 /*====   import Sctipt    ====*/
     import React from 'react';
+    import Text from "../../../../../../items/Text/Text";
 /*====   import Sctipt    ====*/
 export default class InfoDescription extends React.Component{
     render() {
         let result =
             (
                 <React.Fragment>
-                    description
+                    <Text tagName='p'>لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ، و با استفاده از طراحان گرافیک است، چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است، و برای شرایط فعلی تکنولوژی مورد نیاز، و کاربردهای متنوع با هدف بهبود ابزارهای کاربردی می باشد، کتابهای زیادی در شصت و سه درصد گذشته</Text>
                 </React.Fragment>
             );
         return result;
