3aab9607812be6c014458a97c322efe2a3a10217
itonedeveloper <itonedeveloper@gmail.com>
2020-03-16 06:00:28 -0400

    [file]{AboutMe.js}-> create about component

---
+++ src/components/Page/index/Main/AboutMe/AboutMe.js
@@ -0,0 +1,17 @@
+/*====   import Sctipt    ====*/
+    import React from 'react';
+/*====   import Sctipt    ====*/
+export default class AboutMe extends React.Component{
+    render() {
+        let result =
+            (
+                <React.Fragment>
+                    <section id='aboutMe'>
+                        abouteme 
+                    </section>
+                </React.Fragment>
+            );
+        return result;
+    }
+
+}
