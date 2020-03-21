e07cd4e5f42edd73a641ba9b4a4d4a4cf6089fd3
itonedeveloper <itonedeveloper@gmail.com>
2020-03-21 04:12:19 -0400

    [file]{Favorits.js}-> create elements

--- src/components/Page/index/Main/AboutMe/AboutContent/AboutInfo/Favorits/Favorits.js
+++ src/components/Page/index/Main/AboutMe/AboutContent/AboutInfo/Favorits/Favorits.js
@@ -1,15 +1,30 @@
 /*====   import Sctipt    ====*/
-    import React from 'react';
+import React from 'react';
 /*====   import Sctipt    ====*/
-export default class Favorits extends React.Component
-{
+export default class Favorits extends React.Component {
     render() {
         let result =
             (
                 <React.Fragment>
-                    IconList
+                    <ul className='dirL' id='favorits'>
+                        <li>
+                            <i className="fal fa-code"></i>
+                        </li>
+                        <li>
+                            <i className="fal fa-list-music"></i>
+                        </li>
+                        <li>
+                            <i className="fal fa-mug-hot"></i>
+                        </li>
+                        <li>
+                            <i className="far fa-coin"></i>
+                        </li>
+                        <li>
+                            <i className="fal fa-burger-soda"></i>
+                        </li>
+                    </ul>
                 </React.Fragment>
             );
-        return result ;
+        return result;
     }
 }
