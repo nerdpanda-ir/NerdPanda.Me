20821d3bdc7a9771cb1bbad635f7fa531f66c994
itonedeveloper <itonedeveloper@gmail.com>
2020-03-21 03:33:58 -0400

    [file]{AboutMe.js}-> remove trash code and import AbutContent component and use !!!

--- src/components/Page/index/Main/AboutMe/AboutMe.js
+++ src/components/Page/index/Main/AboutMe/AboutMe.js
@@ -1,16 +1,8 @@
 /*====   import Sctipt    ====*/
     import React from 'react';
     import GrayBig from "../../../../items/Title/GrayBig/GrayBig";
-    import Col from "../../../../items/Col/Col";
-    import Container from "../../../../Container/Container";
-    import PerpleBig from "../../../../items/Title/PerpleBig/PerpleBig";
-    import VectorImg from "../../../../items/VectorImg/VectorImg";
-    import BigParagraph from "../../../../items/Paragraph/BigParagraph/BigParagraph";
+    import AboutContent from "./AboutContent/AboutContent";
 /*====   import Sctipt    ====*/
-
-/*====   import Files    ====*/
-    import aboutImg from '../../../../../img/page/index/about.svg'
-/*====   import Files    ====*/
 export default class AboutMe extends React.Component{
     render() {
         let result =
@@ -20,24 +12,7 @@ export default class AboutMe extends React.Component{
                         <GrayBig tagName='h2'>
                            درباره من
                         </GrayBig>
-                        <Container>
-                            <Col className='col-6 flR'>
-                                <PerpleBig tagName='h3'>
-                                    محمد رضا شمس
-                                </PerpleBig>
-                                <section id='localtion'>
-                                    <i className="fal fa-map-marker-smile"></i>
-                                    <span id='country'>ایران</span>
-                                    <span id='city'>اصفهان</span>
-                                </section>
-                                <BigParagraph className='textCenter'>
-                                    لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ، و با استفاده از طراحان گرافیک است، چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است، و برای شرایط فعلی تکنولوژی مورد نیاز، و کاربردهای متنوع با هدف بهبود ابزارهای کاربردی می باشد، کتابهای زیادی در شصت و سه درصد گذشته
-                                </BigParagraph>
-                            </Col>
-                            <Col className='col-6 flR  dirL'>
-                                <VectorImg src={aboutImg} alt='aboutMe' />
-                            </Col>
-                        </Container>
+                        <AboutContent />
                     </section>
                 </React.Fragment>
             );
