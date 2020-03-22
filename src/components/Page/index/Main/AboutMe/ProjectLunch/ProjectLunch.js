b1e05971b3d48499b8526169c978823338573223
itonedeveloper <itonedeveloper@gmail.com>
2020-03-22 08:55:27 -0400

    [file]{ProjectLunch.js}=> create project lunch content

--- src/components/Page/index/Main/AboutMe/ProjectLunch/ProjectLunch.js
+++ src/components/Page/index/Main/AboutMe/ProjectLunch/ProjectLunch.js
@@ -1,6 +1,9 @@
 /*====   import Sctipt    ====*/
     import React from "react";
     import GrayBig from "../../../../../items/Title/GrayBig/GrayBig";
+    import Container from "../../../../../Container/Container";
+    import Col from "../../../../../items/Col/Col";
+    import Tip from "./Tip/Tip";
 /*====   import Sctipt    ====*/
 
 /*====   import Style   ====*/
@@ -16,6 +19,16 @@ export default class ProjectLunch extends React.Component{
                         <GrayBig tagName='h2' className='disIB col-12'>
                             یک پروژه چگونه لانچ می شود
                         </GrayBig>
+                        <Container>
+                            <Col className='col-6 disIB'>
+                                <Tip />
+                                <Tip />
+                            </Col>
+                            <Col className='col-6 disIB dirL'>
+                                <Tip />
+                                <Tip />
+                            </Col>
+                        </Container>
                     </section>
                 </React.Fragment>
             );
