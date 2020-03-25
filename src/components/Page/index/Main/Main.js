026f12d213a7b7ed45cb09afc9fdd97c7d152985
itonedeveloper <itonedeveloper@gmail.com>
2020-03-25 05:55:42 -0400

    [file]{Main.js}-> sort codes

--- src/components/Page/index/Main/Main.js
+++ src/components/Page/index/Main/Main.js
@@ -1,7 +1,7 @@
 /*====   import Sctipt    ====*/
     import React from "react";
     import AboutMe from "./AboutMe/AboutMe";
-import Skill from "./Skill/Skill";
+    import Skill from "./Skill/Skill";
 /*====   import Sctipt    ====*/
 export default class Main extends React.Component{
     render() {
