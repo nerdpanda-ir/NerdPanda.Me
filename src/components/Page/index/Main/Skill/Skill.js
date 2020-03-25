2ecf1cc3221a697404da70e5f785f6742301ec03
itonedeveloper <itonedeveloper@gmail.com>
2020-03-25 05:50:25 -0400

    [file]{Skill.js}-> use Container component and set class name !!! and use Skill Group Component

--- src/components/Page/index/Main/Skill/Skill.js
+++ src/components/Page/index/Main/Skill/Skill.js
@@ -1,7 +1,8 @@
 /*====   import Sctipt    ====*/
     import React from "react";
-import GrayBig from "../../../../items/Title/GrayBig/GrayBig";
-import Container from "../../../../Container/Container";
+    import GrayBig from "../../../../items/Title/GrayBig/GrayBig";
+    import Container from "../../../../Container/Container";
+    import SkillGroup from "./SkillGroup/SkillGroup";
 /*====   import Sctipt    ====*/
 export default class Skill extends React.Component{
     render() {
@@ -10,8 +11,8 @@ export default class Skill extends React.Component{
                 <React.Fragment>
                     <section id='skill'>
                         <GrayBig tagName='h2'> مهارت های من </GrayBig>
-                        <Container>
-                            container 
+                        <Container className='boxSBB'>
+                            <SkillGroup />
                         </Container>
                     </section>
                 </React.Fragment>
