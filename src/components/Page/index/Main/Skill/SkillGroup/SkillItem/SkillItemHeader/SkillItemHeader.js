da8148ba4604670005da4297110935a17ba8f30a
itonedeveloper <itonedeveloper@gmail.com>
2020-03-26 06:13:04 -0400

    [file]{SkillItemHeader.js}-> use stylesheet

--- src/components/Page/index/Main/Skill/SkillGroup/SkillItem/SkillItemHeader/SkillItemHeader.js
+++ src/components/Page/index/Main/Skill/SkillGroup/SkillItem/SkillItemHeader/SkillItemHeader.js
@@ -4,6 +4,12 @@
     import SkillItemTitle from "./SkillItemTitle/SkillItemTitle";
     import SkillItemPerecent from "./SkillItemPerecent/SkillItemPerecent";
 /*====  import Script   ====*/
+
+
+/*====  import Style   ====*/
+    import '../../../../../../../../css/Page/index/Main/Skill/SkillGroup/SkillItem/SkillItemHeader/SkillItemHeader.css'
+/*====  import Style   ====*/
+
 export default class SkillItemHeader extends React.Component{
     render() {
         let result =
