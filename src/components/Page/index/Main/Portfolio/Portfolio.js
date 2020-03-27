09923a6e59ffcb7438cdb83806011d838c633cc0
itonedeveloper <itonedeveloper@gmail.com>
2020-03-27 09:28:14 -0400

    [file]{Portfilo.js}-> create Portfilo component

---
+++ src/components/Page/index/Main/Portfolio/Portfolio.js
@@ -0,0 +1,16 @@
+/*====   import Sctipt    ====*/
+    import React from 'react';
+/*====   import Sctipt    ====*/
+export default class Portfolio extends React.Component{
+    render() {
+        let result =
+            (
+                <React.Fragment>
+                    <section id='Portfolio'>
+                        Portfolio
+                    </section>
+                </React.Fragment>
+            );
+        return result;
+    }
+}
