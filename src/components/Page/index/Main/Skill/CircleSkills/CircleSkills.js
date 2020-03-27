9ec95164add1015ed035538295d0af94fe5935ba
itonedeveloper <itonedeveloper@gmail.com>
2020-03-27 08:59:11 -0400

    [file]{CircleSkills.js}-> create circle Skills Component s

---
+++ src/components/Page/index/Main/Skill/CircleSkills/CircleSkills.js
@@ -0,0 +1,33 @@
+/*====   import Sctipt    ====*/
+    import React from 'react';
+    import CircleProgress from "../../../../../items/CircleProgress/CircleProgress";
+    import {GlobalContextProvider} from "../../../../../GlobalContext/GlobalContext";
+/*====   import Sctipt    ====*/
+
+/*====   import Style    ====*/
+    import '../../../../../../css/Page/index/Main/Skill/CircleSkills/CircleSkills.css'
+/*====   import Style    ====*/
+export default class CircleSkills extends React.Component{
+    render() {
+        let result =
+            (
+                <React.Fragment>
+                    <section className='circleProgressWrapper dirL textCenter'>
+                        <GlobalContextProvider value={{title : 'Oop' , precent : '70%'}}>
+                            <CircleProgress />
+                        </GlobalContextProvider>
+                        <GlobalContextProvider value={{title : 'Git' , precent : '50%'}}>
+                            <CircleProgress />
+                        </GlobalContextProvider>
+                        <GlobalContextProvider value={{title : 'Seo' , precent : '30%'}}>
+                            <CircleProgress />
+                        </GlobalContextProvider>
+                        <GlobalContextProvider value={{title : 'Marketing' , precent : '30%'}}>
+                            <CircleProgress />
+                        </GlobalContextProvider>
+                    </section>
+                </React.Fragment>
+            );
+        return result;
+    }
+}
