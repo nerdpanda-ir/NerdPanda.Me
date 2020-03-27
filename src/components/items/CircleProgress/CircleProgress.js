5f758198b45fba6c924ac192bed38f1d1a115304
itonedeveloper <itonedeveloper@gmail.com>
2020-03-27 09:03:54 -0400

    [file]{CircleProgress.js}=> get data in the React context Api

--- src/components/items/CircleProgress/CircleProgress.js
+++ src/components/items/CircleProgress/CircleProgress.js
@@ -1,5 +1,7 @@
 /*====  Import Script   ====*/
     import React from "react";
+    import GraySmall from "../Title/GraySmall/GraySmall";
+    import {GlobalContextConsumer} from "../../GlobalContext/GlobalContext";
 /*====  Import Script   ====*/
 
 /*====  Import Style   ====*/
@@ -10,13 +12,33 @@ export default class CircleProgress extends React.Component{
             let result =
                 (
                     <React.Fragment>
-                        <div className="progress-circle  p10 ">
-                            <span>99%</span>
-                            <div className="left-half-clipper">
-                                <div className="first50-bar"></div>
-                                <div className="value-bar"></div>
-                            </div>
-                        </div>
+                        <GlobalContextConsumer>
+                            {
+                                value =>
+                                {
+                                    let precent = value.precent.replace('%','');
+                                    precent = precent+' '+((precent>50) ? 'over50' : '');
+                                    let result =
+                                        (
+                                            <React.Fragment>
+                                                <section className='circleProgress  col-2 disIB'>
+                                                    <div className={"margCenter progress-circle  p"+precent}>
+                                                        <span>{value.precent}</span>
+                                                        <div className="left-half-clipper">
+                                                            <div className="first50-bar"></div>
+                                                            <div className="value-bar"></div>
+                                                        </div>
+                                                        <GraySmall tagName='h4' className='posAb circleProgressTitle col-12 t100'> {value.title}</GraySmall>
+                                                    </div>
+                                                </section>
+                                            </React.Fragment>
+                                        );
+                                    return result;
+
+                                }
+                            }
+                        </GlobalContextConsumer>
+
                     </React.Fragment>
                 );
             return result;
