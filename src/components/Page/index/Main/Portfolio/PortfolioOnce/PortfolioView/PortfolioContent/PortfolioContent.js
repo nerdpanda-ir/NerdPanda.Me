a1a51fc935644a11b9a6d7fa5882a8dafc6b902c
itonedeveloper <itonedeveloper@gmail.com>
2020-03-28 11:15:11 -0400

    [file]{PortfolioContent.js} -> create PortfolioContent Component

---
+++ src/components/Page/index/Main/Portfolio/PortfolioOnce/PortfolioView/PortfolioContent/PortfolioContent.js
@@ -0,0 +1,26 @@
+/*====   import Sctipt    ====*/
+    import React from "react";
+    import PortfolioHeader from "./PortfolioHeader/PortfolioHeader";
+    import PortfolioFooter from "./PortfolioFooter/PortfolioFooter";
+/*====   import Sctipt    ====*/
+
+
+/*====   import Style   ====*/
+    import '../../../../../../../../css/Page/index/Main/Portfolio/PortfolioOnce/PortfolioView/PortfolioContent/PortfolioContent.css';
+/*====   import Style   ====*/
+
+export default class PortfolioContent extends React.Component {
+
+    render() {
+        let element =
+            (
+                <React.Fragment>
+                    <figcaption className='portfolioContent boxSBB posAb color-white  col-11 br3 sh4 disN bg-perple-rgba8 dirR'>
+                        <PortfolioHeader />
+                        <PortfolioFooter />
+                    </figcaption>
+                </React.Fragment>
+            );
+        return element
+    }
+}
