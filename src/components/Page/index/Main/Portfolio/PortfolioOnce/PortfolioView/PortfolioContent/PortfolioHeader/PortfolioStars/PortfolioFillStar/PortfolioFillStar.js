11a24fc9135087c34956edfe8f45e4d290ec7b39
itonedeveloper <itonedeveloper@gmail.com>
2020-03-28 11:26:58 -0400

    [file]{PortfolioFillStars.js}-> craete Fill stars Component for Portfolio

---
+++ src/components/Page/index/Main/Portfolio/PortfolioOnce/PortfolioView/PortfolioContent/PortfolioHeader/PortfolioStars/PortfolioFillStar/PortfolioFillStar.js
@@ -0,0 +1,16 @@
+/*====   import Sctipt    ====*/
+    import React from "react";
+/*====   import Sctipt    ====*/
+
+export default class PortfolioFillStar extends React.Component {
+
+    render() {
+        let element =
+            (
+                <React.Fragment>
+                    <li><i className="fas fa-star"></i></li>
+                </React.Fragment>
+            );
+        return element
+    }
+}
