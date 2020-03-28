8f462e8cf4ff8e6c72cf2a6007da0557d2083bb2
itonedeveloper <itonedeveloper@gmail.com>
2020-03-28 11:28:48 -0400

    [file]{PortfolioStrokeStars.js}-> craete STROKE stars Component for Portfolio

---
+++ src/components/Page/index/Main/Portfolio/PortfolioOnce/PortfolioView/PortfolioContent/PortfolioHeader/PortfolioStars/PortfolioStrokeStar/PortfolioStrokeStar.js
@@ -0,0 +1,16 @@
+/*====   import Sctipt    ====*/
+    import React from "react";
+/*====   import Sctipt    ====*/
+
+export default class PortfolioStrokeStar extends React.Component {
+
+    render() {
+        let element =
+            (
+                <React.Fragment>
+                    <li><i className="fal fa-star"></i></li>
+                </React.Fragment>
+            );
+        return element
+    }
+}
