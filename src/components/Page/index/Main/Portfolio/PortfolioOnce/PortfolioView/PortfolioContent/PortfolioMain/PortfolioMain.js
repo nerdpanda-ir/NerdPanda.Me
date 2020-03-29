bc315ed88f17bd280684bf0502b881f42adad9d4
itonedeveloper <itonedeveloper@gmail.com>
2020-03-29 06:42:40 -0400

    [file]{PortfolioMain.js}-> use stylesheet and modify css class names

--- src/components/Page/index/Main/Portfolio/PortfolioOnce/PortfolioView/PortfolioContent/PortfolioMain/PortfolioMain.js
+++ src/components/Page/index/Main/Portfolio/PortfolioOnce/PortfolioView/PortfolioContent/PortfolioMain/PortfolioMain.js
@@ -2,12 +2,16 @@
     import React from "react";
     import PortfolioLike from "./PortfolioLike/PortfolioLike";
 /*====   import Sctipt    ====*/
+
+/*====   import Style   ====*/
+    import '../../../../../../../../../css/Page/index/Main/Portfolio/PortfolioOnce/PortfolioView/PortfolioContent/PortfolioMain/PortfolioMain.css'
+/*====   import Style   ====*/
 export default class PortfolioMain extends React.Component{
     render() {
         let result =
             (
                 <React.Fragment>
-                    <section className='PortfolioMain textCenter'>
+                    <section className='PortfolioMain textCenter posAb boxSBB col-12'>
                         <PortfolioLike />
                     </section>
                 </React.Fragment>
