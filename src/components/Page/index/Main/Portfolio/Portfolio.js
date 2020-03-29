d1e6f516e94760ece79b4b9cb3ceb8532309f907
itonedeveloper <itonedeveloper@gmail.com>
2020-03-29 07:12:58 -0400

    [file]{Portfolio.js}-> use Context Api for PortfolioOnce !!! import portfolio images

--- src/components/Page/index/Main/Portfolio/Portfolio.js
+++ src/components/Page/index/Main/Portfolio/Portfolio.js
@@ -3,14 +3,19 @@
     import GrayBig from "../../../../items/Title/GrayBig/GrayBig";
     import Container from "../../../../Container/Container";
     import PortfolioOnce from "./PortfolioOnce/PortfolioOnce";
+    import {GlobalContextProvider} from "../../../../GlobalContext/GlobalContext";
 /*====   import Sctipt    ====*/
 
 /*====   import Style    ====*/
     import '../../../../../css/Page/index/Main/Portfolio/Portfolio.css'
 /*====   import Style    ====*/
 
-
-
+/*====   import FIles    ====*/
+    import port1 from  '../../../../../img/page/index/Portfolio/port1.jpg'
+    import port2 from  '../../../../../img/page/index/Portfolio/port2.jpg'
+    import port3 from  '../../../../../img/page/index/Portfolio/port3.png'
+    import port4 from  '../../../../../img/page/index/Portfolio/port4.jpg'
+/*====   import FIles    ====*/
 
 
 export default class Portfolio extends React.Component{
@@ -22,10 +27,18 @@ export default class Portfolio extends React.Component{
                         <GrayBig tagName='h2'> نمونه کار ها  </GrayBig>
                         <Container>
                             <ul id='portfolioWrapper'>
-                                <PortfolioOnce />
-                                <PortfolioOnce />
-                                <PortfolioOnce />
-                                <PortfolioOnce />
+                                <GlobalContextProvider value = {{title : 'نمونه کار اول ' , img : port1 , alt : 'نمونه کار اولی '}}>
+                                    <PortfolioOnce />
+                                </GlobalContextProvider>
+                                <GlobalContextProvider value = {{title : 'نمونه کار اول ' , img : port2 , alt : 'نمونه کار اولی '}}>
+                                    <PortfolioOnce />
+                                </GlobalContextProvider>
+                                <GlobalContextProvider value = {{title : 'نمونه کار اول ' , img : port3 , alt : 'نمونه کار اولی '}}>
+                                    <PortfolioOnce />
+                                </GlobalContextProvider>
+                                <GlobalContextProvider value = {{title : 'نمونه کار اول ' , img : port4 , alt : 'نمونه کار اولی '}}>
+                                    <PortfolioOnce />
+                                </GlobalContextProvider>
                             </ul>
                         </Container>
                     </section>
