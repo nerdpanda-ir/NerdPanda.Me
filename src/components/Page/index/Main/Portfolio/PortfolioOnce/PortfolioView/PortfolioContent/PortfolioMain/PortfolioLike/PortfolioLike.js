a4d4e1ccf808c969c58216ffe1ff98545b627754
itonedeveloper <itonedeveloper@gmail.com>
2020-03-29 05:49:08 -0400

    [file]{PortfolioLike.js}-> create PortfolioLike Component

---
+++ src/components/Page/index/Main/Portfolio/PortfolioOnce/PortfolioView/PortfolioContent/PortfolioMain/PortfolioLike/PortfolioLike.js
@@ -0,0 +1,14 @@
+/*====   import Sctipt    ====*/
+    import React from "react";
+/*====   import Sctipt    ====*/
+export default class PortfolioLike extends React.Component{
+    render() {
+        let result =
+            (
+                <React.Fragment>
+                    <i className="fal fa-heart"></i>
+                </React.Fragment>
+            );
+        return result;
+    }
+}
