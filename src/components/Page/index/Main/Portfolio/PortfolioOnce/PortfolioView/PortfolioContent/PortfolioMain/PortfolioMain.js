76a80607a67e0636a1404bb0db0040c3665a4d67
itonedeveloper <itonedeveloper@gmail.com>
2020-03-29 05:35:59 -0400

    [file]{PortfolioMain.js}-> create portfolio main Component

---
+++ src/components/Page/index/Main/Portfolio/PortfolioOnce/PortfolioView/PortfolioContent/PortfolioMain/PortfolioMain.js
@@ -0,0 +1,16 @@
+/*====   import Sctipt    ====*/
+    import React from "react";
+/*====   import Sctipt    ====*/
+export default class PortfolioMain extends React.Component{
+    render() {
+        let result =
+            (
+                <React.Fragment>
+                    <section className='PortfolioMain'>
+                        hello im main !!!!
+                    </section>
+                </React.Fragment>
+            );
+        return result;
+    }
+}
