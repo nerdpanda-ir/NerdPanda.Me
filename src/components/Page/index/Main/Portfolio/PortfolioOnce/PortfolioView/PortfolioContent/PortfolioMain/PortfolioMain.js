390cf0a88eca97ec17a085ee78d5950d2d601352
itonedeveloper <itonedeveloper@gmail.com>
2020-03-29 06:16:02 -0400

    [file]{PortfolioMain.js}-> use PortFolioLike Component

--- src/components/Page/index/Main/Portfolio/PortfolioOnce/PortfolioView/PortfolioContent/PortfolioMain/PortfolioMain.js
+++ src/components/Page/index/Main/Portfolio/PortfolioOnce/PortfolioView/PortfolioContent/PortfolioMain/PortfolioMain.js
@@ -1,13 +1,14 @@
 /*====   import Sctipt    ====*/
     import React from "react";
+    import PortfolioLike from "./PortfolioLike/PortfolioLike";
 /*====   import Sctipt    ====*/
 export default class PortfolioMain extends React.Component{
     render() {
         let result =
             (
                 <React.Fragment>
-                    <section className='PortfolioMain'>
-                        hello im main !!!!
+                    <section className='PortfolioMain textCenter'>
+                        <PortfolioLike />
                     </section>
                 </React.Fragment>
             );
