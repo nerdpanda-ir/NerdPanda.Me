f71d50421d6ade2b85fb69ddbf7c980d09bba76a
itonedeveloper <itonedeveloper@gmail.com>
2020-03-30 07:52:01 -0400

    [file]{PostContent.js}-> attach stylesheet

--- src/components/Page/index/Main/Blog/Post/PostContent/PostContent.js
+++ src/components/Page/index/Main/Blog/Post/PostContent/PostContent.js
@@ -1,9 +1,14 @@
 /*====   import Sctipt    ====*/
     import React from 'react';
-import PostContentHeader from "./PostContentHeader/PostContentHeader";
-import PostContentMain from "./PostContentMain/PostContentMain";
-import PostContentFooter from "./PostContentFooter/PostContentFooter";
+    import PostContentHeader from "./PostContentHeader/PostContentHeader";
+    import PostContentMain from "./PostContentMain/PostContentMain";
+    import PostContentFooter from "./PostContentFooter/PostContentFooter";
 /*====   import Sctipt    ====*/
+
+/*====   import Style    ====*/
+    import '../../../../../../../css/Page/index/Main/Blog/Post/PostContent/PostContent.css'
+/*====   import Style    ====*/
+
 export default class PostContent extends React.Component{
     render() {
         let result =
