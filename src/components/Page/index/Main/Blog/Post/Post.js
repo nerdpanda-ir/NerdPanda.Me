c2dfdb02e1c573cd93323d9bb70f7ea522865087
itonedeveloper <itonedeveloper@gmail.com>
2020-03-30 07:48:29 -0400

    [file]{Post.js}-> attach stylesheet

--- src/components/Page/index/Main/Blog/Post/Post.js
+++ src/components/Page/index/Main/Blog/Post/Post.js
@@ -3,6 +3,10 @@
     import PostImage from "./PostImage/PostImage";
     import PostContent from "./PostContent/PostContent";
 /*====   import Sctipt    ====*/
+
+/*====   import Style    ====*/
+    import '../../../../../../css/Page/index/Main/Blog/Post/Post.css'
+/*====   import Style    ====*/
 export default class Post extends React.Component{
     render() {
         let result =
