a85211aceb5d0cb62faf56fa4a77f7afba30478c
itonedeveloper <itonedeveloper@gmail.com>
2020-03-30 08:03:58 -0400

    [file]{PostContentHeader.js}-> attach stylesheet

--- src/components/Page/index/Main/Blog/Post/PostContent/PostContentHeader/PostContentHeader.js
+++ src/components/Page/index/Main/Blog/Post/PostContent/PostContentHeader/PostContentHeader.js
@@ -3,6 +3,11 @@
     import GraySmall from "../../../../../../../items/Title/GraySmall/GraySmall";
     import {GlobalContextConsumer} from "../../../../../../../GlobalContext/GlobalContext";
 /*====   import Sctipt    ====*/
+
+/*====   import Style    ====*/
+    import '../../../../../../../../css/Page/index/Main/Blog/Post/PostContent/PostContentHeader/PostContentHeader.css'
+/*====   import Style    ====*/
+
 export default class PostContentHeader extends React.Component{
     render() {
         let result =
