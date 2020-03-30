c594508313ac467af63e7d18e2c2645c7dd32921
itonedeveloper <itonedeveloper@gmail.com>
2020-03-30 08:07:13 -0400

    [file]{PostCOntentMain.js}-> attach stylesheet

--- src/components/Page/index/Main/Blog/Post/PostContent/PostContentMain/PostContentMain.js
+++ src/components/Page/index/Main/Blog/Post/PostContent/PostContentMain/PostContentMain.js
@@ -3,6 +3,11 @@
     import BigParagraph from "../../../../../../../items/Paragraph/BigParagraph/BigParagraph";
     import {GlobalContextConsumer} from "../../../../../../../GlobalContext/GlobalContext";
 /*====   import Sctipt    ====*/
+
+/*====   import Style    ====*/
+    import '../../../../../../../../css/Page/index/Main/Blog/Post/PostContent/PostContentMain/PostContentMain.css'
+/*====   import Style    ====*/
+
 export default  class PostContentMain extends React.Component
 {
     render() {
