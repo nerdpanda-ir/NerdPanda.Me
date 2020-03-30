df3f12d5a513987ebcac2029c87423c09bf61421
itonedeveloper <itonedeveloper@gmail.com>
2020-03-30 05:59:45 -0400

    [file]{Post.js}-> remove trash codes and add PostImage Component And PostContent Component

--- src/components/Page/index/Main/Blog/Post/Post.js
+++ src/components/Page/index/Main/Blog/Post/Post.js
@@ -1,9 +1,7 @@
 /*====   import Sctipt    ====*/
     import React from 'react';
-import GraySmall from "../../../../../items/Title/GraySmall/GraySmall";
-import BigParagraph from "../../../../../items/Paragraph/BigParagraph/BigParagraph";
-import MoreBtn from "../../../../../items/MoreBtn/MoreBtn";
-import Container from "../../../../../Container/Container";
+    import PostImage from "./PostImage/PostImage";
+    import PostContent from "./PostContent/PostContent";
 /*====   import Sctipt    ====*/
 export default class Post extends React.Component{
     render() {
@@ -12,36 +10,8 @@ export default class Post extends React.Component{
                 <React.Fragment>
                     <section className='col-6 disIB'>
                         <article className='col-11 bg-white sh3 br5 posRel boxSBB'>
-                            <section className='articleImage'>
-                                <img src="http://localhost:3000/static/media/port4.b22a9f42.jpg" alt="alt" className='col-9 posAb sh5 br5'/>
-                            </section>
-                            <section className='articleContent boxSBB'>
-                                <header className='disIB col-12'>
-                                    <GraySmall tagName='h3'>پست اول </GraySmall>
-                                </header>
-                                <main>
-                                    <BigParagraph>
-                                        لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ، و با استفاده از طراحان گرافیک است، چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است....
-                                    </BigParagraph>
-                                </main>
-                                <footer className='disIB dirL textLeft col-12 boxSBB'>
-                                    <ul>
-                                        <li>
-                                            <i className="fal fa-calendar-edit"></i>
-                                            <span className='iconLable'>1398/10/02</span>
-                                        </li>
-                                        <li>
-                                            <i className="fal fa-comments-alt"></i>
-                                            <span className='iconLable'>50</span>
-                                        </li>
-                                        <li>
-                                            <i className="fal fa-heart"></i>
-                                            <span className='iconLable'>50</span>
-                                        </li>
-                                    </ul>
-                                    <MoreBtn />
-                                </footer>
-                            </section>
+                            <PostImage />
+                            <PostContent />
                         </article>
                     </section>
                 </React.Fragment>
