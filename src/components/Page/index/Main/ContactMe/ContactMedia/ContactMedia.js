4ca1b3ffa912341458022bcfb5af68868edfeac0
itonedeveloper <itonedeveloper@gmail.com>
2020-04-02 04:09:39 -0400

    [file]{ContactMedia.js}-> create ContactMedia Component

---
+++ src/components/Page/index/Main/ContactMe/ContactMedia/ContactMedia.js
@@ -0,0 +1,20 @@
+/*====   import Sctipt    ====*/
+    import React from "react";
+    import Col from "../../../../../items/Col/Col";
+    import VectorImg from "../../../../../items/VectorImg/VectorImg";
+    import contactImg from "../../../../../../img/page/index/contactMe.svg";
+/*====   import Sctipt    ====*/
+    export default class ContactMedia extends React.Component
+    {
+        render() {
+            let result =
+                (
+                    <React.Fragment>
+                        <Col className='col-6 flR dirL' id='contactMedia'>
+                            <VectorImg src={contactImg} alt='contactMe'/>
+                        </Col>
+                    </React.Fragment>
+                );
+            return result;
+        }
+    }
