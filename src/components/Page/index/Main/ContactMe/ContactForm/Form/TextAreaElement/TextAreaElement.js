3734b6dae9821f60cf72a549c28adc57dc21347c
itonedeveloper <itonedeveloper@gmail.com>
2020-04-02 07:33:37 -0400

    [file]{TextAreaElement.js}-> create TextArea Component for use Form !!!!

---
+++ src/components/Page/index/Main/ContactMe/ContactForm/Form/TextAreaElement/TextAreaElement.js
@@ -0,0 +1,20 @@
+/*====   import Sctipt    ====*/
+    import React from "react";
+    import Col from "../../../../../../../items/Col/Col";
+    import {GlobalContextConsumer} from "../../../../../../../GlobalContext/GlobalContext";
+/*====   import Sctipt    ====*/
+export default class TextAreaElement extends React.Component{
+    render() {
+        let result=
+            (
+                    <Col className='inputWrapper posRel col-10'>
+                        <GlobalContextConsumer>
+                            {
+                                value=><React.Fragment><textarea id={value.input.id} className='brN col-12 boxSBB'/><label htmlFor={value.input.id} className='posAb'><i className={value.lable.icon}></i><span className='contactLable'>{value.lable.text}</span></label></React.Fragment>
+                            }
+                        </GlobalContextConsumer>
+                    </Col>
+            );
+        return result;
+    }
+}
