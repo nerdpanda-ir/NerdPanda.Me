847f2b5524896f3eba74bad552e189968837684f
itonedeveloper <itonedeveloper@gmail.com>
2020-04-02 07:06:48 -0400

    [file]{Form.js}-> use InputElement Component and use BtnElement Component and use TextArea Component !!!! and set preventDefalt for never submiting Form Element !!!!

--- src/components/Page/index/Main/ContactMe/ContactForm/Form/Form.js
+++ src/components/Page/index/Main/ContactMe/ContactForm/Form/Form.js
@@ -1,66 +1,45 @@
 /*====   import Sctipt    ====*/
     import React from "react";
-    import Col from "../../../../../../items/Col/Col";
-    import DropDown from "../../../../../../../plugins/DropDown/DropDown";
+    import InputElement from "./InputElement/InputElement";
+    import {GlobalContextProvider} from "../../../../../../GlobalContext/GlobalContext";
+    import TextAreaElement from "./TextAreaElement/TextAreaElement";
+    import DropDownElement from "./DropDownElement/DropDownElement";
+    import BtnElement from "./BtnElement/BtnElement";
 /*====   import Sctipt    ====*/
 export default class Form extends React.Component{
+    constructor() {
+        super();
+        this.elements ={formElement : React.createRef()}
+    }
     render() {
         let result =
             (
                 <React.Fragment>
-                    <form className='col-11' autoComplete='off'>
-                        <Col className='inputWrapper posRel col-10'>
-                            <input type="text" id='email' className='col-12 boxSBB'/>
-                            <label htmlFor='email' className='posAb'>
-                                <i className="fal fa-envelope-open"></i>
-                                <span className='contactLable'>
-                                                ایمیل
-                                     </span>
-                            </label>
-                        </Col>
-                        <Col className='inputWrapper col-10 posRel' id='optionsWrapper'>
-                            <DropDown setting={{selected : {id: "contactOptionSelect", defaltSelect :3, imgClass : 'contactOptionsIcon'}, arrow : {id : 'contactArrow'}, list : {id : 'contactOptions'  , lablelClass : 'contactOptionLable' , imgClass : 'contactOptionsIcon'}}}>
-                                <section className='sh2 br5' id='contactOptionSelect'>
-                                    <i className="fal fa-sort-down color-secound flL" id="contactArrow"></i>
-                                </section>
-                                <ul id='contactOptions' className='disN col-12 sh6 bg-white boxSBB posAb'>
-                                    <li>
-                                        <i className="fal fa-theater-masks  contactOptionsIcon"></i>
-                                        <span className="contactOptionLable ">پیشنهادات و انتقادات </span>
-                                    </li>
-                                    <li>
-                                        <i className="fal fa-lightbulb-on contactOptionsIcon"></i>
-                                        <span className="contactOptionLable">پروژه</span>
-                                    </li>
-                                    <li>
-                                        <i className="fal fa-comments-alt-dollar contactOptionsIcon"></i>
-                                        <span className="contactOptionLable">مشورت</span>
-                                    </li>
-                                    <li>
-                                        <i className="fal fa-handshake  contactOptionsIcon"></i>
-                                        <span className="contactOptionLable">همکاری</span>
-                                    </li>
-                                </ul>
-                            </DropDown>
-                        </Col>
-                        <Col className='inputWrapper posRel col-10'>
-                            <textarea id='msg' className='col-12 boxSBB'/>
-                            <label htmlFor='msg' className='posAb'>
-                                <i className="fal fa-comment-lines"></i>
-                                <span className='contactLable'>
-                                                پیام
-                                     </span>
-                            </label>
-                        </Col>
-                        <Col className='inputWrapper posRel col-10 textCenter'>
-                            <button className='btn bg-perple-dark color-white col-5 brN sh4'>
-                                <i className="fal fa-paper-plane"></i>
-                                ارسال پیام
-                            </button>
-                        </Col>
+                    <form className='col-11' autoComplete='off' ref={this.elements.formElement}>
+                        <GlobalContextProvider value={{lable : {icon :'fal fa-user', text :'نام' } , input : {id : 'nickName'}}}>
+                            <InputElement />
+                        </GlobalContextProvider>
+                        <GlobalContextProvider value={{lable : {icon :'fal fa-envelope-open', text :'ایمیل' } , input : {id : 'email'}}}>
+                            <InputElement />
+                        </GlobalContextProvider>
+                        <DropDownElement />
+                        <GlobalContextProvider value={{lable : {icon :'fal fa-comment-lines', text :'پیام' } , input : {id : 'msg'}}}>
+                            <TextAreaElement />
+                        </GlobalContextProvider>
+                        <GlobalContextProvider value={{icon :'fal fa-paper-plane' , text : 'ارسال'}}>
+                            <BtnElement />
+                        </GlobalContextProvider>
                     </form>
                 </React.Fragment>
             );
         return result;
+    }
+    componentDidMount() {
+        this.elements.formElement.current.addEventListener('submit',this.formSubmitHandler)
+    }
+    formSubmitHandler(ev)
+    {
+        ev.preventDefault();
+        window.alert('متاسفانه ارتباط با سرور برقرار نیست لطفا بعدا تلاش کنید !!!!‌by')
     }
 }
