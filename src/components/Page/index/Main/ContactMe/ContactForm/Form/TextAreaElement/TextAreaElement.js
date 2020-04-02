91ed55d72b34d6908b91eb40a02cd1a79158b8ca
itonedeveloper <itonedeveloper@gmail.com>
2020-04-02 07:38:21 -0400

    [file]{TextAreaElement.js}-> use Stylesheet

--- src/components/Page/index/Main/ContactMe/ContactForm/Form/TextAreaElement/TextAreaElement.js
+++ src/components/Page/index/Main/ContactMe/ContactForm/Form/TextAreaElement/TextAreaElement.js
@@ -3,6 +3,11 @@
     import Col from "../../../../../../../items/Col/Col";
     import {GlobalContextConsumer} from "../../../../../../../GlobalContext/GlobalContext";
 /*====   import Sctipt    ====*/
+
+/*====   import Style   ====*/
+    import '../../../../../../../../css/Page/index/Main/ContactMe/ContactForm/Form/TextAreaElement/TextAreaElement.css'
+/*====   import Style   ====*/
+
 export default class TextAreaElement extends React.Component{
     render() {
         let result=
