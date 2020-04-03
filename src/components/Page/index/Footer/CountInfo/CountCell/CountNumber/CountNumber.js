f5f88610324d1af0d2ba0c78b845c3e3ca26f3e0
itonedeveloper <itonedeveloper@gmail.com>
2020-04-03 05:37:43 -0400

    [file]{CountNumber.js}-> use stylesheet

--- src/components/Page/index/Footer/CountInfo/CountCell/CountNumber/CountNumber.js
+++ src/components/Page/index/Footer/CountInfo/CountCell/CountNumber/CountNumber.js
@@ -1,6 +1,10 @@
 /*====   import Sctipt    ====*/
     import React from 'react';
 /*====   import Sctipt    ====*/
+
+/*====   import Style   ====*/
+    import '../../../../../../../css/Page/index/Footer/CountInfo/CountCell/CountNumber/CountNumber.css'
+/*====   import Style   ====*/
 export default class CountNumber extends React.Component{
     render() {
         let result =
