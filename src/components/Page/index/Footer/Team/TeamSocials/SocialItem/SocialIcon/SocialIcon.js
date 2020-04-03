9ea24cbb390c2b3d21178c509f8a462aa75d2ab2
itonedeveloper <itonedeveloper@gmail.com>
2020-04-03 11:43:07 -0400

    [file]{SocialIcon.css}-> use stylesheet file !!!!

--- src/components/Page/index/Footer/Team/TeamSocials/SocialItem/SocialIcon/SocialIcon.js
+++ src/components/Page/index/Footer/Team/TeamSocials/SocialItem/SocialIcon/SocialIcon.js
@@ -2,6 +2,10 @@
     import React from 'react';
     import {GlobalContextConsumer} from "../../../../../../../GlobalContext/GlobalContext";
 /*====   import Sctipt    ====*/
+
+/*====   import Style    ====*/
+    import '../../../../../../../../css/Page/index/Footer/Team/TeamSocials/SocialItem/SocialIcon/SocialIcon.css'
+/*====   import Style    ====*/
 export default class SocialIcon extends React.Component{
     render() {
         let result =
