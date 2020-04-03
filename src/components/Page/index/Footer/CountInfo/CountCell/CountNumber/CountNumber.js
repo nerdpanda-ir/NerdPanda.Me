cd111db73c8a45c6546e6e003383eaca3218a8bd
itonedeveloper <itonedeveloper@gmail.com>
2020-04-03 04:51:14 -0400

    [file]{CountNumber.js}-> create CountNumber Component for use in the CountCell Component

---
+++ src/components/Page/index/Footer/CountInfo/CountCell/CountNumber/CountNumber.js
@@ -0,0 +1,14 @@
+/*====   import Sctipt    ====*/
+    import React from 'react';
+/*====   import Sctipt    ====*/
+export default class CountNumber extends React.Component{
+    render() {
+        let result =
+            (
+                <React.Fragment>
+                        <span className='countNumber color-secound'>500</span>
+                </React.Fragment>
+            );
+        return result;
+    }
+}
