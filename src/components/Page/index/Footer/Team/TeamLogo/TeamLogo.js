5f37e1d047db41c0bf423460ba0fa69972f0d56b
itonedeveloper <itonedeveloper@gmail.com>
2020-04-03 10:48:16 -0400

    [file]{TeamLogo.js}-> create TeamLogo Component

---
+++ src/components/Page/index/Footer/Team/TeamLogo/TeamLogo.js
@@ -0,0 +1,25 @@
+/*====   import Sctipt    ====*/
+    import React from 'react';
+    import Logo from "../../../../../items/Logo/Logo";
+    import GraySmall from "../../../../../items/Title/GraySmall/GraySmall";
+/*====   import Sctipt    ====*/
+
+/*====   import files    ====*/
+    import teamLogo from "../../../../../../img/global/luxapps_logo.svg";
+/*====   import files    ====*/
+
+export default class TeamLogo extends React.Component{
+    render() {
+        let result =
+            (
+                <React.Fragment>
+                    <figure id='team' className='textCenter'>
+                        <Logo imageSrc={teamLogo} className='col-1 disIB' alt='luxApps.ir'/>
+                        <GraySmall tagName='h2' id='teamTitle'> Lux Apps</GraySmall>
+                        <figcaption>لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ، و با استفاده از طراحان گرافیک است.</figcaption>
+                    </figure>
+                </React.Fragment>
+            );
+        return result;
+    }
+}
