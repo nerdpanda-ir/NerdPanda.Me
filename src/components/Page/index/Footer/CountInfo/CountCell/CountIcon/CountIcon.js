b9c676dbe7255096ac10d8fd3c5b5879025b5e13
itonedeveloper <itonedeveloper@gmail.com>
2020-04-03 04:50:53 -0400

    [file]{CountIcon.js}-> create CountIcon Component for use in the CountCell Component

---
+++ src/components/Page/index/Footer/CountInfo/CountCell/CountIcon/CountIcon.js
@@ -0,0 +1,16 @@
+/*====   import Sctipt    ====*/
+    import React from 'react';
+
+/*====   import Sctipt    ====*/
+export default class CountIcon extends React.Component{
+    render() {
+        let result =
+            (
+                <React.Fragment>
+                        <i className="fal fa-laugh-wink color-secound"></i>
+
+                </React.Fragment>
+            );
+        return result;
+    }
+}
