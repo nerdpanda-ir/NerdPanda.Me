edfe5bc6b3a62b83224500f4457c66481571e1df
itonedeveloper <itonedeveloper@gmail.com>
2020-04-03 10:49:21 -0400

    [file]{TeamSocials.js}-> create teamSocials Component

---
+++ src/components/Page/index/Footer/Team/TeamSocials/TeamSocials.js
@@ -0,0 +1,33 @@
+/*====   import Sctipt    ====*/
+    import React from 'react';
+    import SocialItem from "./SocialItem/SocialItem";
+    import {GlobalContextProvider} from "../../../../../GlobalContext/GlobalContext";
+/*====   import Sctipt    ====*/
+export default class TeamSocials extends React.Component
+{
+    render() {
+        let result =
+            (
+                <React.Fragment>
+                    <ul id='teamSocial' className='textCenter dirL'>
+                        <GlobalContextProvider value={{icon :  'fab fa-facebook-f', link : 'https://www.facebook.com'}}>
+                            <SocialItem />
+                        </GlobalContextProvider>
+                        <GlobalContextProvider value={{icon :  'fab fa-twitter', link : 'https://www.facebook.com'}}>
+                            <SocialItem />
+                        </GlobalContextProvider>
+                        <GlobalContextProvider value={{icon :  'fab fa-instagram', link : 'https://www.facebook.com'}}>
+                            <SocialItem />
+                        </GlobalContextProvider>
+                        <GlobalContextProvider value={{icon :  'fab fa-youtube', link : 'https://www.facebook.com'}}>
+                            <SocialItem />
+                        </GlobalContextProvider>
+                        <GlobalContextProvider value={{icon :  'fab fa-twitch', link : 'https://www.facebook.com'}}>
+                            <SocialItem />
+                        </GlobalContextProvider>
+                    </ul>
+                </React.Fragment>
+            );
+        return result;
+    }
+}
