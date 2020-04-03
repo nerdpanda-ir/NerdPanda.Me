b5940ece04afa951959ebc43958a10542dad09f1
itonedeveloper <itonedeveloper@gmail.com>
2020-04-03 04:52:18 -0400

    [file]{CountInfo.js}-> use CountCell Component

--- src/components/Page/index/Footer/CountInfo/CountInfo.js
+++ src/components/Page/index/Footer/CountInfo/CountInfo.js
@@ -1,6 +1,6 @@
 /*====   import Sctipt    ====*/
     import React from 'react';
-    import GraySmall from "../../../../items/Title/GraySmall/GraySmall";
+    import CountCell from "./CountCell/CountCell";
 /*====   import Sctipt    ====*/
 export default class CountInfo extends React.Component
 {
@@ -8,12 +8,8 @@ export default class CountInfo extends React.Component
         let result =
             (
                 <React.Fragment>
-                    <ul className='sh2 bg-white' id='countInfo'>
-                        <li className='disIB countCell'>
-                            <section className="fal fa-laugh-wink"></section>
-                            <GraySmall className='disB' tagName='strong'>مشتریان راضی </GraySmall>
-                            <section className='countNumber color'>500</section>
-                        </li>
+                    <ul className='sh2 bg-white br5 boxSBB' id='countInfo'>
+                        <CountCell />
                     </ul>
                 </React.Fragment>
             );
