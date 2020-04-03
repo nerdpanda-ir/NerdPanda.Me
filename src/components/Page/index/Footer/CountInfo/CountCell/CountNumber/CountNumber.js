d150ccf9d6849f14e67d74ec1a4c9188c0517dd7
itonedeveloper <itonedeveloper@gmail.com>
2020-04-03 05:53:56 -0400

    [file]{CountNumber.js}-> use Context Api

--- src/components/Page/index/Footer/CountInfo/CountCell/CountNumber/CountNumber.js
+++ src/components/Page/index/Footer/CountInfo/CountCell/CountNumber/CountNumber.js
@@ -1,16 +1,22 @@
 /*====   import Sctipt    ====*/
     import React from 'react';
+    import {GlobalContextConsumer} from "../../../../../../GlobalContext/GlobalContext";
 /*====   import Sctipt    ====*/
 
 /*====   import Style   ====*/
     import '../../../../../../../css/Page/index/Footer/CountInfo/CountCell/CountNumber/CountNumber.css'
+import GraySmall from "../../../../../../items/Title/GraySmall/GraySmall";
 /*====   import Style   ====*/
 export default class CountNumber extends React.Component{
     render() {
         let result =
             (
                 <React.Fragment>
-                        <span className='countNumber color-secound'>500</span>
+                        <span className='countNumber color-secound'>
+                           <GlobalContextConsumer>
+                                {value => value.number}
+                            </GlobalContextConsumer>
+                        </span>
                 </React.Fragment>
             );
         return result;
