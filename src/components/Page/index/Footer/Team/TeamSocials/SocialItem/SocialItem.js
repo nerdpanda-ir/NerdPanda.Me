2c9832567ba4b5a0cc6232d58752f8daaf48667e
itonedeveloper <itonedeveloper@gmail.com>
2020-04-03 10:50:10 -0400

    [file]{SocialItem.js}-> create SocialItem Component

---
+++ src/components/Page/index/Footer/Team/TeamSocials/SocialItem/SocialItem.js
@@ -0,0 +1,22 @@
+/*====   import Sctipt    ====*/
+    import React from "react";
+    import SocialIcon from "./SocialIcon/SocialIcon";
+    import {GlobalContextConsumer} from "../../../../../../GlobalContext/GlobalContext";
+/*====   import Sctipt    ====*/
+export default class SocialItem extends React.Component{
+    render() {
+        let result =
+            (
+                <React.Fragment>
+                    <li className='disIB'>
+                        <GlobalContextConsumer>
+                            {
+                                value=><a href={value.link} className='color-main disIB h100 col-12 br100 textCenter boxSBB bg-white-dark2'><SocialIcon /></a>
+                            }
+                        </GlobalContextConsumer>
+                    </li>
+                </React.Fragment>
+            );
+        return result;
+    }
+}
