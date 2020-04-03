84ebb9800d34a8c9f74bfd67cd9b5e80df355cc7
itonedeveloper <itonedeveloper@gmail.com>
2020-04-03 05:36:54 -0400

    [file]{CountIcon.js}-> use Stylesheet

--- src/components/Page/index/Footer/CountInfo/CountCell/CountIcon/CountIcon.js
+++ src/components/Page/index/Footer/CountInfo/CountCell/CountIcon/CountIcon.js
@@ -1,14 +1,16 @@
 /*====   import Sctipt    ====*/
     import React from 'react';
-
 /*====   import Sctipt    ====*/
+
+/*====   import Style    ====*/
+    import '../../../../../../../css/Page/index/Footer/CountInfo/CountCell/CountIcon/CountIcon.css'
+/*====   import Style    ====*/
 export default class CountIcon extends React.Component{
     render() {
         let result =
             (
                 <React.Fragment>
                         <i className="fal fa-laugh-wink color-secound"></i>
-
                 </React.Fragment>
             );
         return result;
