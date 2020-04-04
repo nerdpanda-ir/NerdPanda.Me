a15cdddb331dc2c72e9b7c9b7631bea54b7ccb3a
itonedeveloper <itonedeveloper@gmail.com>
2020-04-04 04:18:17 -0400

    [file]{CopyRightVersion.js}-> create CopyRightVersion Component

---
+++ src/components/Page/index/Footer/CopyRight/CopyRightVersion/CopyRightVersion.js
@@ -0,0 +1,17 @@
+/*====   import Sctipt    ====*/
+    import React from 'react';
+/*====   import Sctipt    ====*/
+export default class CopyRightVersion extends React.Component{
+    render() {
+        let result =
+            (
+                <React.Fragment>
+                    <span className='posAb l0'>
+                        Alpha Version
+                    </span>
+                </React.Fragment>
+            )
+        ;
+        return result;
+    }
+}
