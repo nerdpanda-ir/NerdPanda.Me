7bbe7656c58560e54243e80018dd5d125210c834
itonedeveloper <itonedeveloper@gmail.com>
2020-04-06 05:19:15 -0400

    [file]{Portfolio.js}-> modify image address and Edit content

--- src/components/Page/index/Main/Portfolio/Portfolio.js
+++ src/components/Page/index/Main/Portfolio/Portfolio.js
@@ -12,10 +12,10 @@
 /*====   import Style    ====*/
 
 /*====   import FIles    ====*/
-    import port1 from  '../../../../../img/page/index/Portfolio/port1.jpg'
+    import port1 from  '../../../../../img/page/index/Portfolio/port1.png'
     import port2 from  '../../../../../img/page/index/Portfolio/port2.jpg'
     import port3 from  '../../../../../img/page/index/Portfolio/port3.png'
-    import port4 from  '../../../../../img/page/index/Portfolio/port4.jpg'
+    import port4 from  '../../../../../img/page/index/Portfolio/port4.png'
 
 /*====   import FIles    ====*/
 
@@ -30,16 +30,16 @@ export default class Portfolio extends React.Component{
                         <Container className='posRel'>
                             <MoreBtn/>
                             <ul id='portfolioWrapper'>
-                                <GlobalContextProvider value = {{title : 'نمونه کار اول ' , img : port1 , alt : 'نمونه کار اولی '}}>
+                                <GlobalContextProvider value = {{title : 'وب سایت  No Shutdown ' , img : port1 , alt : 'وب سایت  No Shutdown '}}>
                                     <PortfolioOnce />
                                 </GlobalContextProvider>
-                                <GlobalContextProvider value = {{title : 'نمونه کار اول ' , img : port2 , alt : 'نمونه کار اولی '}}>
+                                <GlobalContextProvider value = {{title : 'فیش حقوقی بازیافت لنجان ' , img : port2 , alt : 'فیش حقوقی بازیافت لنجان '}}>
                                     <PortfolioOnce />
                                 </GlobalContextProvider>
-                                <GlobalContextProvider value = {{title : 'نمونه کار اول ' , img : port3 , alt : 'نمونه کار اولی '}}>
+                                <GlobalContextProvider value = {{title : 'پالت رنگ برای طراحان' , img : port3 , alt : 'پالت رنگ برای طراحان '}}>
                                     <PortfolioOnce />
                                 </GlobalContextProvider>
-                                <GlobalContextProvider value = {{title : 'نمونه کار اول ' , img : port4 , alt : 'نمونه کار اولی '}}>
+                                <GlobalContextProvider value = {{title : 'فروشگاه فایل' , img : port4 , alt : 'فروشگاه فایل'}}>
                                     <PortfolioOnce />
                                 </GlobalContextProvider>
                             </ul>
