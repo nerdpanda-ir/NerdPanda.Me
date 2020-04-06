2dbe52c86d4d7588d6ec5c19995ccb9061e6b6e4
itonedeveloper <itonedeveloper@gmail.com>
2020-04-06 05:24:03 -0400

    [file]{LinerSkills.js}-> add more skillGroup

--- src/components/Page/index/Main/Skill/LinerSkills/LinerSkills.js
+++ src/components/Page/index/Main/Skill/LinerSkills/LinerSkills.js
@@ -27,11 +27,39 @@ export default class LinerSkills extends React.Component{
                             <SkillItem />
                         </GlobalContextProvider>
                     </SkillGroup>
-                    <SkillGroup title='Back End ' icon='fal fa-lightbulb-on'>
+                    <SkillGroup title='Back End ' icon='fal fa-sliders-h'>
                         <GlobalContextProvider value={{title : 'Php',precent : '70%'}}>
                             <SkillItem />
                         </GlobalContextProvider>
                         <GlobalContextProvider value={{title : 'MySql',precent : '70%'}}>
+                            <SkillItem />
+                        </GlobalContextProvider>
+                        <GlobalContextProvider value={{title : 'Rest Api',precent : '49%'}}>
+                            <SkillItem />
+                        </GlobalContextProvider>
+                    </SkillGroup>
+                    <SkillGroup title='Cms ' icon='fad fa-rocket-launch'>
+                        <GlobalContextProvider value={{title : 'WordPress',precent : '40%'}}>
+                            <SkillItem />
+                        </GlobalContextProvider>
+                        <GlobalContextProvider value={{title : 'Prestashop',precent : '40%'}}>
+                            <SkillItem />
+                        </GlobalContextProvider>
+                        <GlobalContextProvider value={{title : ' DataLife Engine',precent : '55%'}}>
+                            <SkillItem />
+                        </GlobalContextProvider>
+                    </SkillGroup>
+                    <SkillGroup title='Softwares ' icon='fad fa-window'>
+                        <GlobalContextProvider value={{title : 'Php Strom',precent : '70%'}}>
+                            <SkillItem />
+                        </GlobalContextProvider>
+                        <GlobalContextProvider value={{title : 'Liber Office',precent : '30%'}}>
+                            <SkillItem />
+                        </GlobalContextProvider>
+                        <GlobalContextProvider value={{title : 'Microsoft Office',precent : '50%'}}>
+                            <SkillItem />
+                        </GlobalContextProvider>
+                        <GlobalContextProvider value={{title : 'Gimp',precent : '30%'}}>
                             <SkillItem />
                         </GlobalContextProvider>
                     </SkillGroup>
