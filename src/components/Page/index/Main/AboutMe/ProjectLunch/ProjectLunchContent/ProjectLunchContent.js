d4f2215bfdcbf1a74ad58db489d4471987b2d00c
itonedeveloper <itonedeveloper@gmail.com>
2020-04-21 06:37:23 -0400

    [file]{ProjectLunchContent.js}-> create Project Lunch Content Component

---
+++ src/components/Page/index/Main/AboutMe/ProjectLunch/ProjectLunchContent/ProjectLunchContent.js
@@ -0,0 +1,32 @@
+/*====   import Sctipt    ====*/
+    import React from 'react';
+    import Col from "../../../../../../items/Col/Col";
+    import Tip from "../Tip/Tip";
+    import {GlobalContextProvider} from "../../../../../../GlobalContext/GlobalContext";
+/*====   import Sctipt    ====*/
+export default class ProjectLunchContent extends React.Component{
+    render() {
+        let result =
+            (
+                <React.Fragment>
+                    <Col className='col-6 flR'>
+                        <GlobalContextProvider value={{title : 'ایده پردازی' ,icon:'fal fa-lightbulb-on' , description : 'لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ، و با استفاده از طراحان گرافیک است، چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است، و برای شرایط فعلی تکنولوژی مورد نیاز، و کاربردهای متنوع با هدف بهبود ابزارهای کاربردی می باشد،'}}>
+                            <Tip />
+                        </GlobalContextProvider>
+                        <GlobalContextProvider value={{title : 'لورم ایپسوم متن ساختگی' ,icon:'fal fa-lightbulb-on' , description : 'لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ، و با استفاده از طراحان گرافیک است، چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است، و برای شرایط فعلی تکنولوژی مورد نیاز، و کاربردهای متنوع با هدف بهبود ابزارهای کاربردی می باشد،'}}>
+                            <Tip />
+                        </GlobalContextProvider>
+                    </Col>
+                    <Col className='col-6 flR dirL'>
+                        <GlobalContextProvider value={{title : 'ایده پردازی' ,icon:'fal fa-lightbulb-on' , description : 'لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ، و با استفاده از طراحان گرافیک است، چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است، و برای شرایط فعلی تکنولوژی مورد نیاز، و کاربردهای متنوع با هدف بهبود ابزارهای کاربردی می باشد،'}}>
+                            <Tip />
+                        </GlobalContextProvider>
+                        <GlobalContextProvider value={{title : 'لورم ایپسوم متن ساختگی' ,icon:'fal fa-lightbulb-on' , description : 'لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ، و با استفاده از طراحان گرافیک است، چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است، و برای شرایط فعلی تکنولوژی مورد نیاز، و کاربردهای متنوع با هدف بهبود ابزارهای کاربردی می باشد،'}}>
+                            <Tip />
+                        </GlobalContextProvider>
+                    </Col>
+                </React.Fragment>
+            );
+        return result;
+    }
+}
