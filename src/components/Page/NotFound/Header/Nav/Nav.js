0f68306c54d4a42c7941fc68bc7d6e110bc9a097
itonedeveloper <itonedeveloper@gmail.com>
2020-06-06 14:57:25 +0430

    [file]{Nav.js}=> create Nav component

---
+++ src/components/Page/NotFound/Header/Nav/Nav.js
@@ -0,0 +1,17 @@
+/*====   import Sctipt    ====*/
+import React from 'react';
+/*====   import Sctipt    ====*/
+export default class Nav extends React.Component{
+    render() {
+        let result =
+            (
+                <React.Fragment>
+                    <nav>
+                        im nav
+                    </nav>
+                </React.Fragment>
+            );
+        return result;
+    }
+
+}
