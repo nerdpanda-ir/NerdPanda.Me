26c0e3e038de88694b6d19e33a0f7684e65f410b
itonedeveloper <itonedeveloper@gmail.com>
2020-06-06 15:02:01 +0430

    [file]{Nav.js}=> import and use Logo component and set logo src

--- src/components/Page/NotFound/Header/Nav/Nav.js
+++ src/components/Page/NotFound/Header/Nav/Nav.js
@@ -1,13 +1,19 @@
 /*====   import Sctipt    ====*/
 import React from 'react';
+import Logo from "../../../../items/Logo/Logo";
 /*====   import Sctipt    ====*/
+
+/*====   import Files   ====*/
+    import pandaLogo from '../../../../../img/global/Logo.svg';
+/*====   import Files   ====*/
+
 export default class Nav extends React.Component{
     render() {
         let result =
             (
                 <React.Fragment>
                     <nav>
-                        im nav
+                        <Logo imageSrc={pandaLogo} alt='pandaLogo' />
                     </nav>
                 </React.Fragment>
             );
