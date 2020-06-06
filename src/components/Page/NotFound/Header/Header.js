93967edf2d541c2d5a2532d41e11757704ed3f0c
itonedeveloper <itonedeveloper@gmail.com>
2020-06-06 14:54:39 +0430

    [file]{notFound/Header.js}=> create NotFound Header Component

---
+++ src/components/Page/NotFound/Header/Header.js
@@ -0,0 +1,17 @@
+/*====   import Sctipt    ====*/
+import React from 'react';
+/*====   import Sctipt    ====*/
+export default class Header extends React.Component{
+    render() {
+        let result =
+            (
+                <React.Fragment>
+                    <header>
+                        hello im header
+                    </header>
+                </React.Fragment>
+            );
+        return result;
+    }
+
+}
