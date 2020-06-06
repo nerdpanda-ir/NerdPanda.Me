a61f835f6b4d69574c576ce29bfa7a722787ecc1
itonedeveloper <itonedeveloper@gmail.com>
2020-06-06 15:39:52 +0430

    [file]{MenuList.js}-> create Menu list Component

---
+++ src/components/Page/NotFound/Header/Nav/MenuList/MenuList.js
@@ -0,0 +1,17 @@
+/*====   import Sctipt    ====*/
+import React from 'react';
+/*====   import Sctipt    ====*/
+export default class menuList extends React.Component{
+    render() {
+        let result =
+            (
+                <ul>
+                    <li>خانه  </li>
+                    <li>وبلاگ</li>
+                    <li>نمونه کار ها </li>
+                    <li>پادکست</li>
+                </ul>
+            );
+        return result;
+    }
+}
