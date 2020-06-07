d392f03e01bdb385e0b2250fdf0ae6522f74e715
itonedeveloper <itonedeveloper@gmail.com>
2020-06-07 10:14:06 +0430

    [file]{MenuList.js}-> set icon name in component props

--- src/components/Page/NotFound/Header/Nav/MenuList/MenuList.js
+++ src/components/Page/NotFound/Header/Nav/MenuList/MenuList.js
@@ -7,10 +7,11 @@ export default class menuList extends React.Component{
         let result =
             (
                 <ul className='margCenter disB wfc'>
-                    <Item title='خانه'/>
-                    <Item title='وبلاگ'/>
-                    <Item title='نمونه کار ها '/>
-                    <Item title='پادکست ها '/>
+                    <Item title='خانه' icon='fal fa-home-heart'/>
+                    <Item title='وبلاگ' icon='fal fa-newspaper'/>
+                    <Item title='نمونه کار ها ' icon='fal fa-handshake'/>
+                    <Item title='تماس با من' icon='fal fa-comments-dollar'/>
+                    <Item title='پادکست ها ' icon='fal fa-microphone-alt'/>
                 </ul>
             );
         return result;
