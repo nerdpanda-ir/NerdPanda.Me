8918937c4d91d3a19574f660f1311a253a5a2a35
itonedeveloper <itonedeveloper@gmail.com>
2020-06-07 10:12:52 +0430

    [file]{Item.js}-> get icon name from props and set to state and use icon element

--- src/components/Page/NotFound/Header/Nav/MenuList/Item/Item.js
+++ src/components/Page/NotFound/Header/Nav/MenuList/Item/Item.js
@@ -4,16 +4,17 @@ import React from "react";
 export default class Item extends React.Component{
     constructor() {
         super();
-        this.state = {title : ''}
+        this.state = {title : '' , icon : ''}
     }
     static getDerivedStateFromProps(props)
     {
-        return {title : props.title};
+        return {title : props.title , icon : props.icon};
     }
     render() {
         let result =
             (
                 <li className='disIB'>
+                    <i className={this.state.icon}></i>
                     {this.state.title}
                 </li>
             );
